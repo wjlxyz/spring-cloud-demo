a4046f555658078e9616991f4ede742eb2f8051b
jinlong <jinlongwang@etekcity.com.cn>
2019-10-22 16:59:35 +0800

    bootstrap eureka-server

--- spring-cloud-eureka-server/src/main/java/com/jinlong/spring/cloud/eureka/server/SpringCloudEurekaServerApplication.java
+++ spring-cloud-eureka-server/src/main/java/com/jinlong/spring/cloud/eureka/server/SpringCloudEurekaServerApplication.java
@@ -1,13 +1,20 @@
 package com.jinlong.spring.cloud.eureka.server;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author jinlong
+ */
 @SpringBootApplication
+@EnableEurekaServer
+@EnableEurekaClient
 public class SpringCloudEurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+        new SpringApplicationBuilder(SpringCloudEurekaServerApplication.class).web(true).run(args);
     }
 
 }
