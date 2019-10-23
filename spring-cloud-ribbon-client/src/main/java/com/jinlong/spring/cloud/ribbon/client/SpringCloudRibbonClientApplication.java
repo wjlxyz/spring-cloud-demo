18c52a58050930435a6c0d922025318d77c804eb
jinlong <jinlongwang@etekcity.com.cn>
2019-10-23 16:45:32 +0800

    add ribbon module

---
+++ spring-cloud-ribbon-client/src/main/java/com/jinlong/spring/cloud/ribbon/client/SpringCloudRibbonClientApplication.java
@@ -0,0 +1,13 @@
+package com.jinlong.spring.cloud.ribbon.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRibbonClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudRibbonClientApplication.class, args);
+    }
+
+}
